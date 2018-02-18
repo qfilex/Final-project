6713dabc60f997c89f9bd8a7e35ae6b2d509fe00
qfilex <qfilex@gmail.com>
2018-02-18 16:16:06 +0200

    First commit of final project

---
+++ config.js
@@ -0,0 +1,15 @@
+//config.js
+/** TWITTER APP CONFIGURATION
+ * consumer_key
+ * consumer_secret
+ * access_token
+ * access_token_secret
+ */
+module.exports = {
+  consumer_key: 'tElcKw0hZ0fmnkV9r4BdaVQqW',  
+  consumer_secret: 'MqShQpETkIoJqg7qbgOGJlRNJG9pnPawuGUbLTbUmDmrpuoKpp',
+  access_token: '442098986-3ZxhCGtkAXlKE3dWKOzg54yuo72bK8OYOGIMGuQ5',  
+  access_token_secret: 'cl1ablTsUXAlrNiW7mqc1ukh0BbrSMycU8VWU3Id3flmp'
+}
+
+//var Twitter = new twit(config);
